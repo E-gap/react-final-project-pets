0b74087544b3aaddd7a3e73dccc9e3a19632441c
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-05-13 19:04:29 +0300

    pages_upd

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,28 +1,31 @@
 import css from './AuthForm.module.css';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthFormButton from 'components/Buttons/AuthButtons/AuthFormButton/AuthFormButton';
 
 const AuthForm = ({ login }) => {
   return (
     <div className={css.container}>
       <form className={css.inputList}>
         <input className={css.input} placeholder="Email" />
-        <input className={css.input} placeholder="Password" />
+        <input
+          className={!login ? css.input : css.inputLastLog}
+          placeholder="Password"
+        />
         <input
           type={!login ? 'visible' : 'hidden'}
-          className={css.input}
+          className={css.inputLastReg}
           placeholder="Confirm password"
         />
-        <button className={!login ? css.formBtnLog : css.formBtnReg}>
-          {!login ? 'Registration' : 'Login'}
-        </button>
+        <AuthFormButton title={!login ? 'Registration' : 'Login'} />
       </form>
       <div className={css.linklist}>
         <p className={css.txtLink}>
           {!login ? 'Already have an account?' : "Don't have an account?"}
         </p>
-        <p href="" className={css.btnLink}>
+        <Link to={!login ? '/login' : '/register'} className={css.btnLink}>
           {!login ? 'Login' : 'Register'}
-        </p>
+        </Link>
       </div>
     </div>
   );
