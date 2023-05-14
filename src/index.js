b722e5cfa6d8266ed37695a372fd99f3b7e78382
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-05-14 18:45:24 +0300

    auth-upd

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store} from './redux/store';
 import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-final-project-pets">
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter basename="/react-final-project-pets">
+         <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
