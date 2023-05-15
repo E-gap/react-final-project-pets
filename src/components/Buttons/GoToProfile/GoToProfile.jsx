d59bf32099f3b09306330745f5a55763ed635486
maks-sova <maxxxhill2k@gmail.com>
2023-05-16 00:09:02 +0300

    кнопки на перевірку

--- src/components/Buttons/GoToProfile/GoToProfile.jsx
+++ src/components/Buttons/GoToProfile/GoToProfile.jsx
@@ -1,11 +1,31 @@
-//import css from './GoToProfile.module.css';
+import PropTypes from 'prop-types';
+import { IoPawOutline } from 'react-icons/io5';
+import css from './GoToProfile.module.css';
 
-const GoToProfile = () => {
+const GoToProfile = ({
+  onClick,
+  favorite = true,
+  big = true,
+  selector = '',
+}) => {
   return (
-    <div>
-      <p>GoToProfile</p>
-    </div>
+    <button
+      className={
+        favorite
+          ? `${css.btn} ${css.btnFavorite} ${selector}`
+          : `${css.btn} ${selector}`
+      }
+      type="button"
+      style={{ width: big ? '248px' : 'auto' }}
+      onClick={onClick}
+    >
+      <p className={css.title}>Go to profile</p>
+      {favorite && <IoPawOutline className={css.icon} />}
+    </button>
   );
 };
-
 export default GoToProfile;
+
+GoToProfile.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
--- src/components/Buttons/RemovePetButton/RemovePetButton.jsx
+++ src/components/Buttons/RemovePetButton/RemovePetButton.jsx
@@ -1,11 +1,27 @@
-//import css from './RemovePetButton.module.css';
+import PropTypes from 'prop-types';
+import { IoTrashOutline, IoTrash } from 'react-icons/io5';
+import css from './RemovePetButton.module.css';
 
-const RemovePetButton = () => {
+const RemovePetButton = ({ onClick, favorite = false, selector = '' }) => {
   return (
-    <div>
-      <p>RemovePetButton</p>
-    </div>
+    <button
+      className={
+        favorite
+          ? `${css.btn} ${css.btnFavorite} ${selector}`
+          : `${css.btn} ${selector}`
+      }
+      type="button"
+      onClick={onClick}
+    >
+      {!favorite && <IoTrashOutline className={css.icon} />}
+      {favorite && <IoTrash className={css.icon} />}
+    </button>
   );
 };
-
 export default RemovePetButton;
+
+RemovePetButton.propTypes = {
+  favorite: PropTypes.bool,
+  onClick: PropTypes.func.isRequired,
+  selector: PropTypes.string,
+};
