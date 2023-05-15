91d587fb765c8c8ffa552de4345f47eeaa28c5bb
E-gap <106596181+E-gap@users.noreply.github.com>
2023-05-15 16:08:21 +0300

    test

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/react-final-project-pets">
+      {/* <BrowserRouter basename="/react-final-project-pets"> */}
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>
 );
