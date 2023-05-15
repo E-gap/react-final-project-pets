69867457aaebcd0a0a451c78c5ec7acff4906c36
E-gap <106596181+E-gap@users.noreply.github.com>
2023-05-15 18:29:18 +0300

    final

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <BrowserRouter basename="/react-final-project-pets"> */}
       <BrowserRouter>
         <App />
       </BrowserRouter>
