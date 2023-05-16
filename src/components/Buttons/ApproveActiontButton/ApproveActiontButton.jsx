a78adde9d27d0ea2c53058cf7b4ce3509cec9ab5
maks-sova <maxxxhill2k@gmail.com>
2023-05-17 01:48:44 +0300

    кнопки

--- src/components/Buttons/ApproveActiontButton/ApproveActiontButton.jsx
+++ src/components/Buttons/ApproveActiontButton/ApproveActiontButton.jsx
@@ -1,11 +1,31 @@
-//import css from './ApproveActionButton.module.css';
+import PropTypes from 'prop-types';
+import { IoTrashOutline } from 'react-icons/io5';
+import css from './ApproveActiontButton.module.css';
 
-const ApproveActionButton = () => {
+const ApproveActiontButton = ({
+  onClick,
+  favorite = true,
+  big = true,
+  selector = '',
+}) => {
   return (
-    <div>
-      <p>ApproveActionButton</p>
-    </div>
+    <button
+      className={
+        favorite
+          ? `${css.btn} ${css.btnFavorite} ${selector}`
+          : `${css.btn} ${selector}`
+      }
+      type="button"
+      style={{ width: 'auto' }}
+      onClick={onClick}
+    >
+      <p className={css.title}>Yes</p>
+      {favorite && <IoTrashOutline className={css.icon} />}
+    </button>
   );
 };
+export default ApproveActiontButton;
 
-export default ApproveActionButton;
+ApproveActiontButton.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
