e0ae2fdbaa43923d776d61c69d9fbea050edecfc
E-gap <106596181+E-gap@users.noreply.github.com>
2023-05-16 07:47:33 +0000

    Deploying to gh-pages from @ E-gap/react-final-project-pets@9850912d1a10468599bf84bedcf5d43636bd7179 🚀

--- static/js/239.196434e7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[239],{6239:function(e,n,a){a.r(n),a.d(n,{default:function(){return i}});var s="Navigation_nav__7WdPp",t="Navigation_navLink__7PcdX",r=a(1087),c=a(3329),i=function(){return(0,c.jsxs)("nav",{className:s,children:[(0,c.jsx)(r.OL,{className:t,to:"/News",children:"News"}),(0,c.jsx)(r.OL,{className:t,to:"/notices",children:"Find pet"}),(0,c.jsx)(r.OL,{className:t,to:"/Our friends",children:"Our frineds"})]})}}}]);
-//# sourceMappingURL=239.196434e7.chunk.js.map
