6e5dbfb62d7c564a9d8879fe21de28ff23be0ca8
E-gap <106596181+E-gap@users.noreply.github.com>
2023-05-18 13:28:14 +0000

    Deploying to gh-pages from @ E-gap/react-final-project-pets@775e03704cf11b8fec39b4e735ab93fa6dc6f2eb 🚀

--- static/js/666.8bacb2c7.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[666],{3666:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var o="NotFound_notFound__6-kto",r=n(3329),u=function(){return(0,r.jsx)("div",{className:o,children:(0,r.jsx)("p",{children:"The page is not found"})})}}}]);
-//# sourceMappingURL=666.8bacb2c7.chunk.js.map
