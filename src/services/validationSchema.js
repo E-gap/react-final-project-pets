fb43d471ea6300b38f8fb9752de8aa4ee1b92eb6
pickfix <Prikhodko.B.T@gmail.com>
2023-05-20 02:24:36 +0300

    upd validate

--- src/services/validationSchema.js
+++ src/services/validationSchema.js
@@ -5,23 +5,44 @@ const validationSchema = {
     category: Yup.string().required('Category is required'),
   }),
   1: Yup.object().shape({
-    // title: Yup.string().required('Title is required'),
-    name: Yup.string().required('Name is required').min(2, 'Too short!').max(16, 'Too long!'),
-    birthday: Yup.string().required('Birthday is required').matches(
-      /^([0-9]{2}).([0-9]{2}).([0-9]{4})$/,
-      'Invalid date format. Use DD.MM.YYYY'
-    ),
-    breed: Yup.string().required('Breed is required').min(2, 'Breed should be at least 2 characters').max(16, 'Breed should not exceed 16 characters'),
+    title: Yup.string().when('category', 
+    { is: Yup.string().oneOf(['sell', 'lost-found', 'good-hands']), then: Yup.string()
+.required('Title is required').min(4,"min 4 exeet")}),
+
+name: Yup.string()
+.required('Name is required')
+.min(2, 'Name must be at least 2 characters')
+.max(16, 'Name must not exceed 16 characters'),
+birthday: Yup.string()
+.required('Date is required')
+.matches(
+  /^(\d{2})\.(\d{2})\.(\d{4})$/,
+  'Invalid date format. Use DD.MM.YYYY'
+),
+breed: Yup.string()
+.required('Breed is required')
+.min(2, 'Breed must be at least 2 characters')
+.max(16, 'Breed must not exceed 16 characters'),
   }),
   2: Yup.object().shape({
-    image: Yup.mixed().test('fileSize', 'File size too large', (value) => !value || value.size <= 5000000),
-    location: Yup.string().required('Location is required'),
-    price: Yup.number().when('category', {
-      is: 'sell',
-      then: Yup.number().required('Price is required').min(1, 'Price should be more than 0'),
+
+    // file: Yup.mixed()
+    // .required('File is required')
+    // .test(
+    //   'fileSize',
+    //   'File size must not exceed 3MB',
+    //   value => value && value.size <= 3 * 1024 * 1024
+    // ),
+    location: Yup.string().when('category', {
+      is: value => ['sell', 'lost-found', 'for-free'].includes(value),
+      then: Yup.string()
+        .required('Location is required')
+        .matches(/^[A-Za-z\s]+$/, 'Invalid location format'),
     }),
-    comments: Yup.string().min(4, 'Comments should be at least 4 characters').max(120, 'Comments should not exceed 120 characters'),
+    comments: Yup.string()
+      .min(4, 'Comments should be at least 4 characters')
+      .max(120, 'Comments should not exceed 120 characters'),
   }),
 };
 
-export default validationSchema;
+export default validationSchema;  
