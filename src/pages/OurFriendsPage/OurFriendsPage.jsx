f4d651160c82e83ae782f773a7e759863725aad2
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-05-21 17:55:28 +0300

    fix bags

--- src/pages/OurFriendsPage/OurFriendsPage.jsx
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -7,30 +7,20 @@ import defaultImg from "./petImg.png"
 const OurFriendsPage = () => {
   const [friends, setFriends] = useState([]);
 
-  const instance = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
-  });
+  useEffect(() => {
+    const instance = axios.create({baseURL: process.env.REACT_APP_API_URL});
 
-  // console.log(friends)
-
-  const getFriends = async () => {
-    try {
-      const  response = await instance.get('/friends');
-      setFriends(response.data);
-      return response.data;
-    } catch (error) {
-      return error.message;
-    }
-    
-  };
-
-useEffect(() => {
-  getFriends();
-},[])
-  
-  const noReload = () => {
-    elements.preventdefault();
-  }
+    const getFriends = async () => {
+      try {
+        const response = await instance.get('/friends');
+        setFriends(response.data);
+        return response.data;
+      } catch (error) {
+        return error.message;
+      }
+    };
+    getFriends();
+  }, []);
 
   const elements = friends.map(item => (
     <li key={item._id} className={css.item}>
@@ -54,22 +44,32 @@ useEffect(() => {
             </a>
           </li>
           <li>
-            <a
-              href={item.email ? `mailto:${item.email}`: noReload}
-              className={css.itemText}
-            >
-              <p className={css.textTitle}>Email:</p>
-              <p>{item.email ? item.email : 'phone only'}</p>
-            </a>
+            {item.email && (
+              <a href={`mailto:${item.email}`} className={css.itemText}>
+                <p className={css.textTitle}>Email:</p>
+                <p>{item.email}</p>
+              </a>
+            )}
+            {!item.email && (
+              <div className={css.itemText}>
+                <p className={css.textTitle}>Email:</p>
+                <p>phone only</p>
+              </div>
+            )}
           </li>
           <li>
-            <a
-              href={item.phone ? `tel:${item.phone}`: noReload}
-              className={css.itemText}
-            >
-              <p className={css.textTitle}>Phone:</p>
-              <p>{item.phone ? item.phone : 'email only'}</p>
-            </a>
+            {item.phone && (
+              <a href={`tel:${item.phone}`} className={css.itemText}>
+                <p className={css.textTitle}>Phone:</p>
+                <p>{item.phone}</p>
+              </a>
+            )}
+            {!item.phone && (
+              <div className={css.itemText}>
+                <p className={css.textTitle}>Phone:</p>
+                <p>email only</p>
+              </div>
+            )}
           </li>
         </ul>
       </div>
--- src/services/TimeSheet/TimeSheet.jsx
+++ src/services/TimeSheet/TimeSheet.jsx
@@ -1,61 +1,86 @@
 import css from './TimeSheet.module.css';
-// import { useState, useEffect} from 'react';
+import {useState, useEffect} from 'react';
 import PropTypes from 'prop-types';
 
 const TimeSheet = ({ workDays }) => {
-  // const [MN, setMN] = useState({});
+  const [state, setState] = useState([]);
 
-  // const monday = () => {
-  //   const day = workDays[1];
-  //   if (day) {
-  //     setMN(day);
-  //     console.log(day);
-  //   } else {
-  //     return
-  //   }
-  // }
- 
-  // useEffect(() => {
-  //   monday();
-  // }, [monday]);
+  const defaultState = [
+    { id: 1, from: 'day', to: 'night' },
+    { id: 2, from: 'day', to: 'night' },
+    { id: 3, from: 'day', to: 'night' },
+    { id: 4, from: 'day', to: 'night' },
+    { id: 5, from: 'day', to: 'night' },
+    { id: 6, from: 'day', to: 'night' },
+    { id: 7, from: 'day', to: 'night' },
+  ];
+
+  const time = state.reduce(
+    (prev, cur) => (cur?.isOpen > prev.isOpen ? cur : prev),
+    {
+      isOpen: -Infinity,
+    }
+  );
+
+  useEffect(() => {
+    if (workDays) {
+      setState(workDays);
+    } else {
+      return;
+    } 
+  }, [workDays]);
+
+  console.log(state)
+  
+  const elements = state.map((item, index) => (
+    <li key={index} className={css.timeItem}>
+      {item.isOpen && (<p>{item.from}-{item.to}</p>)}
+      {!item.isOpen && <p className={css.itemClose}>Close</p>}
+    </li>
+  ));
+
+  const defaultElements = defaultState.map(item => (
+    <li key={item.id} className={css.timeItem}>
+      <p>
+        {item.from} and {item.to}
+      </p>
+    </li>
+  ));
 
   return (
     <>
       <button className={css.timeText}>
         <p className={css.textTitle}>Time:</p>
-        <p>8:00-19:00</p>
+
+        <p>{time.from ? `${time.from} - ${time.to}` : 'day and night'}</p>
       </button>
       <div className={css.timeContainer}>
-        <ul className={css.timeList}>
-          <li className={css.timeItem}>
-            <p>MN</p>
-            <p>8:00-19:00</p>
-          </li>
-          <li className={css.timeItem}>
-            <p>TU</p>
-            <p>8:00-19:00</p>
-          </li>
-          <li className={css.timeItem}>
-            <p>WE</p>
-            <p>8:00-19:00</p>
-          </li>
-          <li className={css.timeItem}>
-            <p>TH</p>
-            <p>8:00-19:00</p>
-          </li>
-          <li className={css.timeItem}>
-            <p>FR</p>
-            <p>8:00-19:00</p>
-          </li>
-          <li className={css.timeItem}>
-            <p>SA</p>
-            <p>8:00-19:00</p>
-          </li>
-          <li className={css.timeItem}>
-            <p>SU</p>
-            <p>8:00-19:00</p>
-          </li>
-        </ul>
+        <div className={css.sheetContainer}>
+          <ul className={css.timeList}>
+            <li className={css.timeItem}>
+              <p>MN</p>
+            </li>
+            <li className={css.timeItem}>
+              <p>TU</p>
+            </li>
+            <li className={css.timeItem}>
+              <p>WE</p>
+            </li>
+            <li className={css.timeItem}>
+              <p>TH</p>
+            </li>
+            <li className={css.timeItem}>
+              <p>FR</p>
+            </li>
+            <li className={css.timeItem}>
+              <p>SA</p>
+            </li>
+            <li className={css.timeItem}>
+              <p>SU</p>
+            </li>
+          </ul>
+          <ul className={css.timeList}>{elements.length ? elements : defaultElements}</ul>
+        </div>
       </div>
     </>
   );
