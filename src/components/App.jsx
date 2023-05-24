17603ed2a5126cef0765401bbc7ffd2dc5212fc7
E-gap <106596181+E-gap@users.noreply.github.com>
2023-05-25 00:00:28 +0300

    update app routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,11 +30,11 @@ export const App = () => {
             <Route path="sell" element={NoticesCategoriesList} />
             <Route path="lost-found" element={NoticesCategoriesList} />
             <Route path="for-free" element={NoticesCategoriesList} />
+            <Route path="/notices/favorite" element={NoticesCategoriesList} />
+            <Route path="/notices/own" element={NoticesCategoriesList} />
           </Route>
           <Route path="*" element={<NotFound />} />
           <Route element={<PrivateRoute />}>
-            <Route path="/notices/favorite" element={NoticesCategoriesList} />
-            <Route path="/notices/own" element={NoticesCategoriesList} />
             <Route path="/add-pet" element={<AddPetForm />} />
             <Route path="/user" element={<UserPage />} />
           </Route>
